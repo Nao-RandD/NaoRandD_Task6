da3d859f9a33531a7d1f1e751ae5293f6ed2d09e
akio0911 <akio0911@gmail.com>
2021-04-08 19:41:02 +0900

    feedback

--- AppDojoTask6/ViewController.swift
+++ AppDojoTask6/ViewController.swift
@@ -9,46 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
-	@IBOutlet weak var label: UILabel!
-	private var sliderValue = 0
-	private var answer = 0
+	@IBOutlet private weak var label: UILabel!
+    @IBOutlet private weak var slider: UISlider!
+    private var answer = 0
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
-		getRandomValue()
-	}
 
-	@IBAction func sliderChanged(_ sender: UISlider) {
-		sliderValue = Int(sender.value * 100)
+        resetGame()
 	}
 
 	@IBAction private func tappedButton(_ sender: Any) {
-		if sliderValue == answer{
-			showAlert(message: "あたり！\nあなたの値: \(sliderValue)")
+        let sliderValue = Int(slider.value)
+
+        let firstLine: String
+
+		if sliderValue == answer {
+            firstLine = "あたり！"
         }else{
-            showAlert(message: "はずれ!\nあなたの値: \(sliderValue)")
+            firstLine = "はずれ!"
         }
+
+        showAlert(message: "\(firstLine)\nあなたの値: \(sliderValue)")
 	}
 
-    private func getRandomValue() {
-        answer = Int(arc4random_uniform(100))
+    private func resetGame() {
+        answer = Int.random(in: 1...100)
         label.text = String(answer)
     }
 
 	// アラートを表示する関数
 	private func showAlert(message: String){
 		// 再調整ボタンの処理
-		let nextAction: UIAlertAction = UIAlertAction(title: "再挑戦", style: UIAlertAction.Style.cancel, handler: { (action: UIAlertAction!) -> Void in
-                                                        self.getRandomValue()
+		let nextAction = UIAlertAction(title: "再挑戦", style: .cancel, handler: { [weak self] _ in
+            self?.resetGame()
         })
-		let alert: UIAlertController = UIAlertController(title: "結果", message: message, preferredStyle:  UIAlertController.Style.alert)
+		let alert = UIAlertController(title: "結果", message: message, preferredStyle: .alert)
 
 		alert.addAction(nextAction)
 
 		present(alert, animated: true, completion: nil)
 	}
-
-
 }
-
